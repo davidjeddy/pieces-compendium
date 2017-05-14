5216e4d281a386eb3c1456f3d613eb0872b4a1a6
David J Eddy <me@davidjeddy.com>
2017-05-14 13:32:52 -0400

    REPLACED boilerplate site name with env('APP_NAME').

--- tests/codeception/frontend/acceptance/HomeCept.php
+++ tests/codeception/frontend/acceptance/HomeCept.php
@@ -6,7 +6,7 @@ use tests\codeception\frontend\AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('Yii2 Starter Kit');
+$I->see(env('APP_NAME'));
 $I->seeLink('About');
 $I->click('About');
 $I->amOnPage('/page/about');
--- tests/codeception/frontend/functional/HomeCept.php
+++ tests/codeception/frontend/functional/HomeCept.php
@@ -6,7 +6,7 @@ use tests\codeception\frontend\FunctionalTester;
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('Yii2 Starter Kit');
+$I->see(env('APP_NAME'));
 $I->seeLink('About');
 $I->click('About');
 $I->see('Lorem ipsum');
